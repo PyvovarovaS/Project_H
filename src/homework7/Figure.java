44caedb6d7e8bc6cef7a75d63b80dd922e9cce45
svetlanawork <pyvovarova.svetlana@gmail.com>
2023-03-05 17:29:39 +0100

    HW_7 fix

--- src/homework7/Figure.java
+++ src/homework7/Figure.java
@@ -13,12 +13,6 @@ public class Figure {
         this.area = area;
     }
 
-    // methods
-    public void printFigure() {
-        System.out.println("Назва фігури: " + name);
-        System.out.println("Площа фігури: " + area);
-    }
-
     //getters
     public String getName() {
         return name;
@@ -30,9 +24,6 @@ public class Figure {
     //setters
     public void setName(String name) {
         this.name = name;
-    }
-    public void setArea(double area) {
-        this.area = area;
     }
 
 }
